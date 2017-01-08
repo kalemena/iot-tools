0c83f4956b829ea8c120a5d850be1bc4f263e01b
kalemena <kalemena@free.fr>
2017-01-08 11:27:10 +0100

    Doc split

---
+++ sketches/teleinfo/teleinfo.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial teleinfo(14, 15);
+//SoftwareSerial teleinfo(2,3);
+
+void setup() {
+  Serial.begin(1200);     // opens serial port, sets data rate to 1200 bps
+  teleinfo.begin(1200);
+
+  Serial.println("Teleinfo module is up...");
+}
+
+int i = 0;
+void loop() {
+  if (teleinfo.available())
+    Serial.write(teleinfo.read() & 0x7F);
+  if(++i % 100000 == 0)
+    Serial.println(".");
+}
+
+
