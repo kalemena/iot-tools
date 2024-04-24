00c87480598381f05d6f4934b1d8df98d274660c
kalemena <kalemena@free.fr>
2024-04-24 08:26:00 +0200

    Update build script

--- .github/build-arduino-ide.sh
+++ .github/build-arduino-ide.sh
@@ -2,32 +2,34 @@
 
 IMAGE=${IMAGE:-kalemena/arduino}
 VERSION=${VERSION:-1.8.19}
+BOARD=${BOARD:-arduino}
 
 # PREPARE
 docker -v
-docker pull ${IMAGE}:${VERSION} || true
+docker pull ${IMAGE}:${VERSION}-${BOARD} || true
 
 # INFO
 echo IMAGE   = ${IMAGE}
 echo VERSION = ${VERSION}
+echo BOARD = ${BOARD}
 
 # BUILD
-echo "Building ${IMAGE}:${VERSION} ..."
+echo "Building ${IMAGE}:${VERSION}-${BOARD} ..."
 
-cd src/main/docker
+cd src/main/docker/boards/${BOARD}
 pwd
 ls -la
 
-docker build --pull --cache-from ${IMAGE}:${VERSION} \
-    -t ${IMAGE}:${VERSION} \
+docker build --pull --cache-from ${IMAGE}:${VERSION}-${BOARD} \
+    -t ${IMAGE}:${VERSION}-${BOARD} \
     -f Dockerfile \
     --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
     --build-arg VCS_REF=`git rev-parse --short HEAD` \
-    --build-arg VERSION=${VERSION} .
+    --build-arg VERSION=${VERSION}-${BOARD} .
 
 # CHECK
 docker ps -a
-docker tag ${IMAGE}:${VERSION} ${IMAGE}:latest
+docker tag ${IMAGE}:${VERSION}-${BOARD} ${IMAGE}:latest
 docker images
 
 # # PUSH
